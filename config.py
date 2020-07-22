90aa3603cd63d1f82cff6ff39a8a512f8df1a2b1
josphat-mwangi <josphatwanjiruw@gmail.com>
2020-07-22 07:23:46 +0300

    create a configuration

---
+++ config.py
@@ -0,0 +1,39 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+
+
+
+class ProdConfig(Config):
+    DEBUG = False
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
