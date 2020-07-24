d8ed05e59746eb6c079a0cec87781605bdb595d9
josphat-mwangi <josphatwanjiruw@gmail.com>
2020-07-24 20:51:57 +0300

    add profile function

--- app/main/views.py
+++ app/main/views.py
@@ -22,5 +22,10 @@ def index():
     return render_template('index.html', title=title)
 
 
-
+@main.route('/user/<uname')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+    return render_template("profile/profile.html", user = user)
 
